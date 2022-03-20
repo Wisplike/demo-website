a72378c1bea745fa593d7aa19e894135f28a3cd2
Wisplike <101737087+Wisplike@users.noreply.github.com>
2022-03-20 07:06:54 +0100

    Add files via upload

---
+++ script.js
@@ -0,0 +1,9 @@
+function mode_dark () {
+    document.getElementById('dark_light_mode').style.color = '#ffffff';
+    document.getElementById('dark_light_mode').style.backgroundColor = '#181818';
+}
+
+function mode_light () {
+    document.getElementById('dark_light_mode').style.color = '#333333';
+    document.getElementById('dark_light_mode').style.backgroundColor = '#ffffff';
+}
